b399b8130ae408ac0782b4ae7ab811ccaa547721
João Junior  Nebauer Schneider <55754251+joaonebauer1@users.noreply.github.com>
2021-06-11 14:52:11 -0300

    Atualizações
    excluidos mais coisas desnecessarias e feito mais caixa na home

--- js/admin.js
+++ js/admin.js
@@ -7,6 +7,112 @@ function time() { // Busca hora em tempo real
     setTimeout('time()', 500);
 }
 
+$.validator.setDefaults({
+    submitHandler: function() {
+        window.open('dashboard.html', '_self', false)
+    }
+});
+
+$().ready(function() {
+    // validate the comment form when it is submitted
+    $("#commentForm").validate();
+
+    // validate signup form on keyup and submit
+    $("#signupForm").validate({
+        rules: {
+            firstname: "required",
+            lastname: "required",
+            username: {
+                required: true,
+                minlength: 1
+            },
+            password: {
+                required: true,
+                minlength: 1
+            },
+            confirm_password: {
+                required: true,
+                minlength: 2,
+                equalTo: "#password"
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            topic: {
+                required: "#newsletter:checked",
+                minlength: 2
+            },
+            agree: "required"
+        },
+        messages: {
+            firstname: "Please enter your firstname",
+            lastname: "Please enter your lastname",
+            username: {
+                required: "Please enter a username",
+                minlength: "Your username must consist of at least 1 character"
+            },
+            password: {
+                required: "Digite sua senha!",
+                minlength: "Your password must be at least 1 character long"
+            },
+
+            email: "Please enter a valid email address",
+            agree: "Please accept our policy"
+        }
+    });
+
+    // propose username by combining first- and lastname
+    $("#username").focus(function() {
+        var firstname = $("#firstname").val();
+        var lastname = $("#lastname").val();
+        if (firstname && lastname && !this.value) {
+            this.value = firstname + "." + lastname;
+        }
+    });
+
+    //code to hide topic selection, disable for demo
+    var newsletter = $("#newsletter");
+    // newsletter topics are optional, hide at first
+    var inital = newsletter.is(":checked");
+    var topics = $("#newsletter_topics")[inital ? "removeClass" : "addClass"]("gray");
+    var topicInputs = topics.find("input").attr("disabled", !inital);
+    // show when newsletter is checked
+    newsletter.click(function() {
+        topics[this.checked ? "removeClass" : "addClass"]("gray");
+        topicInputs.attr("disabled", !this.checked);
+    });
+});
+//----------------------------------------------------------
+$(document).ready(function() {
+
+    $("#btn-blog-next").click(function() {
+        $('#blogCarousel').carousel('next')
+    });
+    $("#btn-blog-prev").click(function() {
+        $('#blogCarousel').carousel('prev')
+    });
+
+    $("#btn-client-next").click(function() {
+        $('#clientCarousel').carousel('next')
+    });
+    $("#btn-client-prev").click(function() {
+        $('#clientCarousel').carousel('prev')
+    });
+
+});
+
+$(window).load(function() {
+
+    $('.flexslider').flexslider({
+        animation: "slide",
+        slideshow: true,
+        start: function(slider) {
+            $('body').removeClass('loading');
+        }
+    });
+});
+
 //---------------------------------------------
 function fazGet(url) {
     let request = new XMLHttpRequest()
--- js/dash-charts.js
+++ js/dash-charts.js
@@ -83,3 +83,43 @@ $(document).ready(function() {
     });
 
 });
+
+$(document).ready(function() {
+    info = new Highcharts.Chart({
+        chart: {
+            renderTo: 'inadinplentes',
+            margin: [0, 0, 0, 0],
+            backgroundColor: null,
+            plotBackgroundColor: 'none',
+
+        },
+
+        title: {
+            text: null
+        },
+
+        tooltip: {
+            formatter: function() {
+                return this.point.name + ': ' + this.y + ' %';
+
+            }
+        },
+        series: [{
+            borderWidth: 2,
+            borderColor: '#F1F3EB',
+            shadow: false,
+            type: 'pie',
+            name: 'SiteInfo',
+            innerSize: '65%',
+            data: [
+                { name: 'Porcentagem', y: 65.0, color: '#fa1d2d' },
+                { name: 'Resta', y: 35.0, color: '#3d3d3d' }
+            ],
+            dataLabels: {
+                enabled: false,
+                color: '#000000',
+                connectorColor: '#000000'
+            }
+        }]
+    });
+});
